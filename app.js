ce09308e5ac4ad7881a4f0e38f6ff06adb5250d8
teataeho <rlaxogh9504@gmail.com>
2023-04-11 12:05:03 +0900

    v0.2

--- app.js
+++ app.js
@@ -8,27 +8,29 @@ document.querySelector('.todo-list').addEventListener('click', e => {
         //수정
     } else if (e.target.matches('.modify')) {
         if (e.target.previousElementSibling.previousElementSibling.checked) {
-            const $modifyInput = document.createElement('input');
-            $modifyInput.value = e.target.previousElementSibling.textContent;
-            e.target.previousElementSibling.classList.add('hide');
-            e.target.insertAdjacentElement('beforebegin', $modifyInput);
-            e.target.textContent = 'ㅁㅁ';
-            e.target.classList.replace('modify', 'ok');
-            e.target.nextElementSibling.classList.replace('delete', 'cancel');
+            const $modifyInput = document.createElement('input'); //인풋요소 생성
+            $modifyInput.value = e.target.previousElementSibling.textContent; //인풋요소의 value에 원래 텍스트 삽입
+            $modifyInput.classList.add('text');
+            e.target.previousElementSibling.classList.add('hide'); //원래 텍스트 숨기기
+            e.target.insertAdjacentElement('beforebegin', $modifyInput); //인풋요소 삽입
+            e.target.classList.replace('lnr-checkmark-circle', 'lnr-undo'); //수정아이콘 변경
+            e.target.classList.replace('modify', 'ok'); //수정버튼의 클래스 확인으로 변경
+            e.target.nextElementSibling.classList.replace('delete', 'cancel'); //삭제버튼의 클래스 취소로 변경
         }
         //수정확인
     } else if (e.target.matches('.ok')) {
-        e.target.previousElementSibling.previousElementSibling.textContent = e.target.previousElementSibling.value;
-        e.target.previousElementSibling.remove();
-        e.target.previousElementSibling.classList.remove('hide');
-        e.target.classList.replace('ok', 'modify');
-        e.target.textContent = 'ㅇㅇ';
+        e.target.previousElementSibling.previousElementSibling.textContent = e.target.previousElementSibling.value; //인풋의 값을 원래 텍스트에 삽입
+        e.target.previousElementSibling.remove(); //인풋 제거
+        e.target.previousElementSibling.classList.remove('hide'); //원래 텍스트 숨기기 해제
+        e.target.classList.replace('ok', 'modify'); //
+        e.target.classList.replace('lnr-undo', 'lnr-checkmark-circle');
+        e.target.nextElementSibling.classList.replace('cancel', 'delete');
         //수정취소
     } else if (e.target.matches('.cancel')) {
         e.target.previousElementSibling.previousElementSibling.remove();
         e.target.previousElementSibling.previousElementSibling.classList.remove('hide');
         e.target.previousElementSibling.classList.replace('ok', 'modify');
         e.target.classList.replace('cancel', 'delete');
-        e.target.previousElementSibling.textContent = 'ㅇㅇ';
+        e.target.previousElementSibling.classList.replace('lnr-undo', 'lnr-checkmark-circle');
     }
 });
